785c0deeb7d19c578d962c312de0f1439c801f9c
williamjin95 <williamjinwork@gmail.com>
2024-05-11 15:14:38 +1000

    new commit break

--- saints-tracker/saints_tracker.py
+++ saints-tracker/saints_tracker.py
@@ -1,17 +1,24 @@
 def main_menu():
     while True:
-        print("\nAFL Team Tracker\n")
+        print("\nAFL Team Tracker")
         print("1. View Team Stats")
         print("2. View Upcoming Games")
-        print("3. Exit")
+        print("3. List Players")
+        print("4. View Player Stats")
+        print("5. Exit")
 
-        choice = input("Enter your choice [1-3]: ")
+        choice = input("Enter your choice [1-5]: ")
 
         if choice == '1':
             view_team_stats()
         elif choice == '2':
             view_upcoming_games()
         elif choice == '3':
+            list_players()
+        elif choice == '4':
+            player_name = input("Enter the name of the player to view stats: ")
+            show_player_stats(player_name)
+        elif choice == '5':
             print("Exiting... Thank you for using the Saints Team Tracker!")
             break
         else:
